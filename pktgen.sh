f50e6d99b9fe76a8b92a2cdab373bee81630aab6
Zhen Ni <leonizhen@gwmail.gwu.edu>
2018-07-16 10:26:24 -0500

    update pktgen script

--- pktgen.sh
+++ pktgen.sh
@@ -1,1 +1,5 @@
-sudo ./app/x86_64-native-linuxapp-gcc/pktgen -l 0,1,2,3,4 -n 3 -- -p 3 -P -m "[1:2].0, [3:4].1"
+sudo ./app/x86_64-native-linuxapp-gcc/pktgen -l 0,1,2,3,4 -n 3 -- -p 3 -P -m "[1:2].0, [3:4].1" -f openNetVM-Scripts/pktgen-config.lua
+
+set 1 dst mac 90:e2:ba:b5:01:f4
+
+set 0 dst mac 90:e2:ba:b5:0f:2c
