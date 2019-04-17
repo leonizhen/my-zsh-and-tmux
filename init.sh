12abde7b9dc2612d1d001169b42ddf1152b85423
Zhen Ni <leonizhen@users.noreply.github.com>
2019-04-17 11:17:30 -0400

    add essential package for NFP

--- init.sh
+++ init.sh
@@ -1,10 +1,8 @@
-sudo apt-get install libnuma-dev vim libpcap0.8 libpcap0.8-dev libpcap-dev && sudo apt-get update && sudo apt-get upgrade
+sudo add-apt-repository ppa:ubuntu-wine/ppa
+sudo add-apt-repository ppa:danielrichter2007/grub-customizer
+
+sudo apt-get install zsh build-essential linux-headers-$(uname -r) git libnuma-dev vim libpcap0.8 libpcap0.8-dev libpcap-dev libftdi1 dkms realpath libjansson4 grub-customizer wine1.8 winetricks ubuntu-desktop && sudo apt-get update && sudo apt-get upgrade
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 git clone https://github.com/leonizhen/my-zsh-and-tmux.git
 cp my-zsh-and-tmux/.zshrc .
 ./my-zsh-and-tmux/tmux.sh
-git clone https://github.com/leonizhen/openNetVM-dev.git
-cd openNetVM-dev
-git checkout isolation
-git pull
-git submodule init && git submodule update
