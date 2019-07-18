1de38b2ed451598e57c1c3e1ed60a277601f9088
Zhen Ni <leonizhen@gwu.edu>
2019-07-18 14:03:24 -0400

    update init.sh for ubuntu 18

--- init.sh
+++ init.sh
@@ -1,8 +1,4 @@
-sudo add-apt-repository ppa:ubuntu-wine/ppa
-sudo add-apt-repository ppa:danielrichter2007/grub-customizer
-
-sudo apt-get install zsh build-essential linux-headers-$(uname -r) git libnuma-dev vim libpcap0.8 libpcap0.8-dev libpcap-dev liblua5.3-dev libftdi1 dkms realpath libjansson4 grub-customizer wine1.8 winetricks ubuntu-desktop && sudo apt-get update && sudo apt-get upgrade
+sudo apt-get update && sudo apt-get install zsh build-essential linux-headers-$(uname -r) git libnuma-dev vim libpcap0.8 libpcap0.8-dev libpcap-dev liblua5.3-dev libftdi1 dkms libjansson4 wine64 ubuntu-desktop && sudo apt-get upgrade
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-git clone https://github.com/leonizhen/my-zsh-and-tmux.git
 cp my-zsh-and-tmux/.zshrc .
 ./my-zsh-and-tmux/tmux.sh
